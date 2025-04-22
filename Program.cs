badf62f792c53f6ae8e0ddfe3836b75305a5e1b7
EnderEzhik <Achernov2007@gmail.com>
2025-04-22 20:31:59 +0300

    add: добавлен шаблон телеграмм бота add: добавлена библиотека Telegram.Bot для работы с апи телеграмм ботов add: добавлена библиотека DotNetEnv для работы с .env add: добавлен .env для хранения токенов

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace RegiBot
+﻿using DotNetEnv;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace RegiBot
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        private static CancellationTokenSource _cts;
+        private static TelegramBotClient _botClient;
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Env.Load();
+            _cts = new CancellationTokenSource();
+            _botClient = new TelegramBotClient(Env.GetString("BOT_TOKEN"), cancellationToken: _cts.Token);
+
+            await _botClient.DeleteWebhook();
+            await _botClient.DropPendingUpdates();
+
+            _botClient.OnMessage += MessageHandler;
+
+            var me = await _botClient.GetMe();
+
+            Console.WriteLine($"Bot {me.Username} is started... Press escape to terminate");
+
+            while(Console.ReadKey(true).Key != ConsoleKey.Escape) { }
+
+            _cts.Cancel();
+        }
+
+        private static async Task MessageHandler(Message message, UpdateType type)
+        {
+
         }
     }
 }
